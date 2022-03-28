49853a245102348a0b95661f147aaf94cc635caa
Арина Короткова <arina.korotkova2002@yandex.ru>
2022-03-28 17:09:26 +0300

    матрицы

--- main.js
+++ main.js
@@ -1,101 +1,109 @@
 ///// 1
-function absValue(x)
-{
-    if (x < 0) {
-        return -x;
-      }
-      return x;
+function absValue(x) {
+  if (x < 0) {
+    return -x;
+  }
+  return x;
 }
 
-console.log(-2, absValue(-2))
+console.log(-2, absValue(-2));
 
 ///// 2
-let slovo = prompt('Введите слово для проверки: ', 'довод');
-function isPalindrome (slovo)
-{
+let slovo = prompt("Введите слово для проверки: ", "довод");
+function isPalindrome(slovo) {
   var ccount = (slovo.length - 1) / 2;
-    for (let x = 0; x < ccount; x++) 
-    {
-      if (slovo[x] != slovo.slice(-1-x)[0]) 
-      {
-        console.log("Данное слово - не палиндром.");
-        return false;
-      }
+  for (let x = 0; x < ccount; x++) {
+    if (slovo[x] != slovo.slice(-1 - x)[0]) {
+      console.log("Данное слово - не палиндром.");
+      return false;
     }
-    console.log("Данное слово - палиндром.");
-    return true;
+  }
+  console.log("Данное слово - палиндром.");
+  return true;
 }
 
-isPalindrome (slovo);
-
+isPalindrome(slovo);
 
 ///// 3
 function matrixAddition(matrix1, matrix2) 
 {
-    if (matrix1.length != matrix2.length || matrix1[0].length != matrix2[0].length)
+  if 
+  (
+    matrix1.length != matrix2.length ||
+    matrix1[0].length != matrix2[0].length
+  ) {
+    console.log("Матрицы не равны. Операция невозможна");
+    return;
+  }
+
+  let result = [];
+
+  for (let i = 0; i < matrix1.length; i++) 
+  {
+    result[i] = [];
+
+    for (let j = 0; j < matrix1.length; j++) 
     {
-        console.log("Матрицы не равны. Операция невозможна");
-        return; 
+      result[i][j] = matrix1[i][j] + matrix2[i][j];
     }
+  }
 
-    let result = [];
-
-    for (let i = 0; i < matrix1.length; i++) 
-    {
-        result[i] = [];
-
-        for (let j = 0; j < matrix1.length; j++) 
-        {
-            result[i][j] = matrix1[i][j] + matrix2[i][j];
-        }
-    }
-
-    return result;
+  return result;
 }
 
-const mat1 = matrixAddition
-    (
-        [
-            [1, 2],
-            [3, 4],
-        ],
-        [
-            [9, 8],
-            [7, 6],
-        ]
-    );
+const mat1 = matrixAddition(
+  [
+    [1, 2],
+    [3, 4],
+  ],
+  [
+    [9, 8],
+    [7, 6],
+  ]
+);
 
-const mat2 = matrixAddition
-    (
-        [
-            [1, 2],
-            [3, 4],
-        ],
-        [
-            [9, 8],
-            [7, 6],
-        ]
-    );
+const mat2 = matrixAddition(
+  [
+    [1, 2],
+    [3, 4],
+  ],
+  [
+    [9, 8],
+    [7, 6],
+  ]
+);
 
-console.log(mat1);
-console.log(mat2);
-console.log(matrixAddition(mat1, mat2));
+// console.log(mat1);
+// console.log(mat2);
 
+let mat = matrixAddition(mat1, mat2)
+
+function matOutput (mat) {
+  matrix = '';
+  for (let i = 0; i < mat.length; i++)
+  {
+    for (let j = 0; j < mat[0].length; j++)
+    {
+      matrix = matrix + mat[i][j] + '\t';
+    }
+    matrix += '\n'
+  }
+  console.log(matrix)
+}
+
+matOutput(mat);
 
 ///// 4
-let student = 
-{
+let student = {
   group: "201-323",
   last_name: "Короткова",
-  first_name: "Арина"
+  first_name: "Арина",
 };
 
 console.log(`Список свойств:\n${Object.keys(student)}`);
-console.log
-(
+console.log(
   `Студент ${student.last_name} ${student.first_name} учится в группе ${student.group}`
 );
-
 
 ///// 5
 let slideIndex = 1;
